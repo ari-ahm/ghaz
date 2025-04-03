cc18cf029a7aa6af9add7ae2e00b241a65508d18
Arian Ahmidi <arian.ahmidi@gmail.com>
2025-04-03 23:32:52 +0330

    fix: goose shouldn't be shown in the taskbar

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,13 @@
 #include <QApplication>
 #include <cstdlib>
 #include <ctime>
+#include <qwidget.h>
 
 int main(int argc, char *argv[]) {
   srand(time(0));
   QApplication app(argc, argv);
-  mainloop goose;
+  QWidget par; // so that it's not shown in the taskbar
+  mainloop goose(&par);
   goose.show();
   return app.exec();
 }
